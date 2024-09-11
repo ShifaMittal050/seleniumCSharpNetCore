4fb7357e51151aab2c041600a8ba1b5f4e810c59
ShifaMittal050 <mstrialaccforcrm1@outlook.com>
2024-09-11 15:40:10 +0530

    Update Hooks1.cs

--- SeleniumCSharpNetCore/Hooks/Hooks1.cs
+++ SeleniumCSharpNetCore/Hooks/Hooks1.cs
@@ -10,10 +10,10 @@ using WebDriverManager.DriverConfigs.Impl;
 namespace SeleniumCSharpNetCore.Hooks
 {
     [Binding]
-    public sealed class Hooks1
+    public sealed class Hooks1 : DriverHelper
     {
-        private DriverHelper _driverHelper;
-        public Hooks1(DriverHelper driverHelper) => _driverHelper = driverHelper;
+       // private DriverHelper _driverHelper;
+       // public Hooks1(DriverHelper driverHelper) => _driverHelper = driverHelper;
 
         [BeforeScenario]
         public void BeforeScenario()
@@ -21,17 +21,19 @@ namespace SeleniumCSharpNetCore.Hooks
             ChromeOptions option = new ChromeOptions();
             option.AddArguments("start-maximized");
             option.AddArguments("--disable-gpu");
-            //option.AddArguments("--headless");
+            option.AddArguments("--headless");
 
             new DriverManager().SetUpDriver(new ChromeConfig());
             Console.WriteLine("Setup");
-            _driverHelper.Driver = new ChromeDriver(option);
+           // _driverHelper.Driver = new ChromeDriver(option);
+            Driver = new ChromeDriver(option);
         }
 
         [AfterScenario]
         public void AfterScenario()
         {
-            _driverHelper.Driver.Quit();
+           // _driverHelper.Driver.Quit();
+            Driver.Quit();
         }
     }
 }
